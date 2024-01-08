b92bb756eacde2f9a74718bc9427c086ace2a667
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-01-08 09:44:54 +0900

    2024-01-08
    - 1000번 문제 풀이 (람다식)

--- src/problem/Problem1000.java
+++ src/problem/Problem1000.java
@@ -1,6 +1,7 @@
 package src.problem;
 
 import java.util.Scanner;
+import java.util.function.BiFunction;
 
 public class Problem1000 {
     public static void main(String args[]) {
@@ -8,7 +9,13 @@ public class Problem1000 {
         int a, b;
         a = sc.nextInt();
         b = sc.nextInt();
-        int result = sumAToB(a,b);
+        // sumAToB 메서드 사용
+        // int result = sumAToB(a,b);
+
+        // sumAToB 메서드 대신 람다식 사용
+        BiFunction<Integer, Integer, Integer> sumAToB = (x, y) -> x + y;
+
+        int result = sumAToB.apply(a, b);
         System.out.println(result);
     }
 
