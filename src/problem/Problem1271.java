d1673fa0311f40e53a718d60de44ee6cfdbed038
MingyuRomeoKim <rlaalsrb0466@naver.com>
2024-01-09 16:51:08 +0900

    2024-01-08 : 1271번 문제
    - 1271번 문제 풀이 (BigInteger)

---
+++ src/problem/Problem1271.java
@@ -0,0 +1,35 @@
+package src.problem;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Problem1271 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n, m;
+        n = getBigInteger(sc);
+        m = getBigInteger(sc);
+
+        BigInteger[] result = n.divideAndRemainder(m);
+
+        System.out.println(result[0]); // 몫
+        System.out.println(result[1]); // 나머지
+    }
+
+    private static BigInteger getBigInteger(Scanner sc) {
+        while (true) {
+            try {
+                BigInteger number = sc.nextBigInteger();
+                if (number.compareTo(BigInteger.ONE) >= 0) {
+                    return number;
+                } else {
+                    System.out.println("1 이상의 정수만 입력 가능합니다.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("정수만 입력 가능합니다.");
+                sc.nextLine(); // 입력 버퍼를 비웁니다.
+            }
+        }
+    }
+}
